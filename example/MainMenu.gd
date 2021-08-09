556d57ec427e79edc655888ab5404bcb4032c849
uberfig <aidan.gifford04@gmail.com>
2021-08-09 09:23:32 -0400

    removed example from addon

--- example/MainMenu.gd
+++
@@ -1,7 +0,0 @@
-extends VBoxContainer
-
-func _on_HostButton_pressed():
-	get_tree().change_scene("res://example/HostGame.tscn")
-
-func _on_BrowseButton_pressed():
-	get_tree().change_scene("res://example/ServerBrowser.tscn")
